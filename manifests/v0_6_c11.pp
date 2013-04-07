efd054447e095f6639033e48eb7664770f8ac841
Danny Berger <dpb587@gmail.com>
2013-04-06 20:47:35 -0600

    first commit

---
+++ manifests/v0_6_c11.pp
@@ -0,0 +1,9 @@
+class pkg-easyinstall::v0_6_c11 {
+    exec {
+        'easy_install' :
+            command => '/usr/bin/wget http://pypi.python.org/packages/2.6/s/setuptools/setuptools-0.6c11-py2.6.egg && /bin/sh setuptools-0.6c11-py2.6.egg',
+            creates => '/usr/bin/easy_install',
+            cwd => '/tmp/',
+            ;
+    }
+}
