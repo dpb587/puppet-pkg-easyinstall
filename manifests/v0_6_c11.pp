17cf1a6513d8d366446700b78f7721b836181aa1
Danny Berger <dpb587@gmail.com>
2013-04-06 22:08:41 -0600

    support deps

--- manifests/v0_6_c11.pp
+++ manifests/v0_6_c11.pp
@@ -1,9 +1,14 @@
 class pkg-easyinstall::v0_6_c11 {
+    include pkg-easyinstall
+
     exec {
         'easy_install' :
             command => '/usr/bin/wget http://pypi.python.org/packages/2.6/s/setuptools/setuptools-0.6c11-py2.6.egg && /bin/sh setuptools-0.6c11-py2.6.egg',
             creates => '/usr/bin/easy_install',
             cwd => '/tmp/',
+            before => [
+                Class['pkg-easyinstall'],
+            ],
             ;
     }
 }
